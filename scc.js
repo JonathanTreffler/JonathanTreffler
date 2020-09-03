b124da6f8b4c9b08f2cad470915b24e1e727fbc0
Jonathan Treffler <mail@jonathan-treffler.de>
2020-09-03 16:51:09 +0200

    modified scc.js

--- scc.js
+++ scc.js
@@ -1,7 +1,33 @@
 fs = require('fs');
 fs.readFile("scc.html", 'utf8', function (err,data) {
-  console.log(data);
+  var scc = data;
 });
 fs.readFile("README.md", 'utf8', function (err,data) {
-  console.log(data);
+  var readme = data;
 });
+
+let startString = "<!-- /start_scc/ -->";
+let endString = "<!-- /end_scc/ -->";
+
+let startIndex = readme.indexOf(startString);
+let endIndex = readme.indexOf(endString);
+
+console.log("=========");
+console.log(startIndex, endIndex);
+
+let betweenString = readme.substring(startIndex, endIndex)
+
+let searchString = startString + betweenString + endString;
+
+console.log("=========");
+console.log(searchString);
+
+let replaceString = startString + scc + endString;
+
+console.log("=========");
+console.log(replaceString);
+
+let result = readme.replace(searchString, replaceString);
+
+console.log("=========");
+console.log(result);
