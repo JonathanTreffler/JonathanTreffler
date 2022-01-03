176cfe4e6972bd36723887e34dd7290e6e9fa065
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2022-01-03 13:20:01 +0000

    moved the scc code into its own directory

--- scc.js
+++
@@ -1,9 +0,0 @@
-fs = require('fs');
-const { updateSection } = require("./file-section-updater/file_updater.js");
-
-fs.readFile("scc.html", 'utf8', function (err,data) {
-  let startString = "<!-- /start_scc/ -->";
-  let endString = "<!-- /end_scc/ -->";
-
-  updateSection("README.md", startString, endString, data);
-});
