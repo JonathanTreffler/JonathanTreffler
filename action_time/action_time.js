e19afb6ce2af7214790e2a9e762842f833ac5b81
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2022-01-03 03:38:30 +0000

    continued developent on action_time

--- action_time/action_time.js
+++ action_time/action_time.js
@@ -3,23 +3,22 @@ fs = require('fs');
 
 const octokit = new Octokit({ auth: process.env.BOT_GITHUB_TOKEN });
 
-async function getRepoTime(user, repository) {
-    console.log("===== " + user + "/" + repository + " =====");
-
-    let summedRunTime = 0;
-
+async function getRepoActionCount(user, repository) {
     const {headers, data: countData} = await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
         owner: user,
         repo: repository,
-        per_page: 100
+        per_page: 1
     })
 
     console.log(headers["x-ratelimit-used"], " / ", headers["x-ratelimit-limit"]);
 
-    let total_count = countData.total_count;
+    let requestsLeft = headers["x-ratelimit-remaining"];
 
-    console.log("total count: ", total_count);
-    console.log("requests needed: ", Math.ceil(total_count/100));
+    return { total_count: countData.total_count, requestsLeft};
+}
+
+async function getRepoTime(user, repository, total_count) {
+    let summedRunTime = 0;
 
     for(let i = 1; i < (total_count / 100) + 1; i++) {
         console.log("Request #" + i);
@@ -49,14 +48,58 @@ async function getRepoTime(user, repository) {
     console.log("... done");
 }
 
-let repositories = JSON.parse(fs.readFileSync("repositories.json"));
+async function run() {
+    var requestsLeft = 0;
+    let repositories = JSON.parse(fs.readFileSync("repositories.json"));
 
-let currentId = parseInt(fs.readFileSync("currentId.txt"));
+    while(true) {
+        let currentId = parseInt(fs.readFileSync("currentId.txt"));
 
-if(currentId >= repositories.length) {
-    currentId = 0;
+        if(currentId >= repositories.length) {
+            currentId = 0;
+            fs.writeFileSync("currentId.txt", "0");
+            break;
+        }
+
+        let user = repositories[currentId][0];
+        let repository = repositories[currentId][1];
+
+        console.log("===== " + user + "/" + repository + " =====");
+
+        let total_count;
+        
+        ({ total_count, requestsLeft } = await getRepoActionCount(user, repository));
+
+        let requestsNeeded = Math.ceil(total_count/100);
+
+        console.log("requestsLeft:", requestsLeft);
+        console.log("requestsNeeded:", requestsNeeded);
+
+        if(requestsLeft > requestsNeeded) {
+            console.log("Enough Requests left")
+            await getRepoTime(user, repository, total_count);
+            fs.writeFileSync("currentId.txt", currentId + 1 + "");
+        } else {
+            console.log("Not enough Requests left")
+            break;
+        }
+    }
 }
 
-fs.writeFileSync("currentId.txt", currentId + 1 + "");
+async function sumUpRepositories() {
+    let repositories = JSON.parse(fs.readFileSync("repositories.json"));
 
-getRepoTime(repositories[currentId][0], repositories[currentId][1]);
+    let sum = 0;
+
+    for(repository of repositories) {
+        let user = repository[0];
+        let repository = repository[1];
+
+        sum += parseInt(fs.readFileSync("./stats/"+user+"_"+repository+".txt"));
+    }
+
+    console.log("Sum all repositories:", sum);
+}
+
+run();
+sumUpRepositories();
